09e230554f2834f99769a47c7f6d441a0afb3850
aayaka <aayaka0719@gmail.com>
2017-06-29 14:53:54 +0900

    Add 138

---
+++ launch/travis_test_lightsensors.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+#encoding: utf8
+import unittest, rostest
+import rosnode, rospy
+import time
+from pimouse_ros.msg import LightSensorValues
+
+class LightsensorTest(unittest.TestCase):
+	def setUp(self):
+		self.count = 0
+		rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+		self.values = LightSensorValues()
+
+	def callback(self, data):
+		self.count += 1
+		self.values = data
+
+	def check_values(self, lf, ls, rs, rf):
+		vs = self.values
+		self.assertEqual(vs.left_forward, lf, "different values: left_forward")
+		self.assertEqual(vs.left_side, ls, "different values: left_side")
+		self.assertEqual(vs.right_side, rs, "different values: right_side")
+		self.assertEqual(vs.right_forward, rf, "different values: right_forward")
+		self.assertEqual(vs.sum_all, lf+ls+rs+rf, "different values: sum_all")
+		self.assertEqual(vs.sum_forward, lf+rf, "different values: sum_forward")
+
+	def test_node_exist(self):
+		nodes = rosnode.get_node_names()
+		self.assertIn('/lightsensors', nodes, "node does not exist")
+
+	def test_get_value(self):
+		rospy.set_param('lightsensors_freq', 10)
+		time.sleep(2)
+		with open("/dev/rtlightsensor0","w") as f:
+			f.write("-1 0 123 4321\n")
+		time.sleep(3)
+		self.assertFalse(self.count == 0,"cannot subscribe the topic")
+		self.check_values(4321,123,0,-1)
+
+	def
+
